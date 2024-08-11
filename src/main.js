9150fa88b0300f8799389ee26a02aa753eec7d8c
leonardo-pesci <leonardo.pesci.2003@gmail.com>
2024-08-11 09:16:44 +0200

    js

--- src/main.js
+++ src/main.js
@@ -1,16 +1,47 @@
 // ^ Elementi
+const html = document.documentElement
+const locationText = document.querySelector('#location')
+const img = document.querySelector('#weatherImg')
+const temperatureText = document.querySelector('#temperatureText')
+const suggestionText = document.querySelector('#suggestionText')
 
 
 
 // ^ Variabili
-
+let suggestions = {
+    '01d': 'Ricordati la crema solare!',
+    '01n': 'Buonanotte!',
+    '02d': 'Oggi il sole va e viene...',
+    '02n': 'Attenti ai lupi mannari...',
+    '03d': 'Luce perfetta per fare foto!',
+    '03n': 'Dormi sereno :)',
+    '04d': 'Che cielo grigio :(',
+    '04n': 'Non si vede nemmeno la luna!',
+    '09d': 'Prendi l\'ombrello',
+    '09n': 'Copriti bene!',
+    '10d': 'Prendi l\'ombrello',
+    '10n': 'Copriti bene!',
+    '11d': 'Attento ai fulmini!',
+    '11n': 'I lampi accendono la notte!',
+    '13d': 'Esci a fare un pupazzo di neve!',
+    '13n': 'Notte perfetta per stare sotto il piumone!',
+    '50d': 'Accendi i fendinebbia!',
+    '50n': 'Guida con prudenza!'
+  };
+  
 
 
 // ^ Funzioni
-
-
-
-// ^ Eventi
+let onError = () => {
+    // Preparo degli elementi in pagina per far capire che va attivata
+    locationText.innerText = '';
+    img.alt = "Geolocation disattivata";
+    img.src = "./img/disabled/geolocationDisabled.png";
+    suggestionText.innerText = 'Attiva la geolocalizzazione'
+  
+    // Disattivare il loading
+    htmlElement.className = '';
+  }
 
 
 
